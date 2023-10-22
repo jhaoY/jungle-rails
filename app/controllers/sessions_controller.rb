6352bf8de53a9b2162ce1a8dd5e6c0b4f6502fd9
Justin Hao <justinhao@live.com>
2023-10-22 17:23:04 -0400

    Add user auth to navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params:[:email])
+    user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect to '/'
+      redirect_to '/'
     else
       redirect_to '/login'
     end
