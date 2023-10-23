0980279e144f7a90bd4d04421d67f3d4a5a7e429
Justin Hao <justinhao@live.com>
2023-10-23 17:37:11 -0400

    Add user method to authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,13 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :password, length: { minimum: 6 }
 
-  # def authenticate_with_credentials(:email, :password)
-    
-  # end
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 
 end
