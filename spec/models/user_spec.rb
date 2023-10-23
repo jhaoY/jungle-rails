563b1636e7c8237346fa1cffc1369764537d0bb9
Justin Hao <justinhao@live.com>
2023-10-23 17:15:50 -0400

    Refactor to use before each

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "Validations" do
+    before :each do
+      @user = User.new(first_name: "Peter", last_name: "Parker", email: "spider@man.com", password: "ilovemj", password_confirmation: 'ilovemj')
+    end
+    
     it "password and password confirmation fields must be the same" do
       @user = User.create(first_name: "Peter", last_name: "Parker", email: "spider@man.com", password: "ilovemj", password_confirmation: 'ilovemj')
 
@@ -17,28 +21,29 @@ RSpec.describe User, type: :model do
     end
 
     it "email must be present" do
-      @user = User.create(first_name: "Peter", last_name: "Parker", email: nil, password: "ilovemj", password_confirmation: 'ilovemj')
+      @user.email = nil
 
       expect(@user).to_not be_valid
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
 
     it "first name must be present" do
-      @user = User.create(first_name: nil, last_name: "Parker", email: "spider@man.com", password: "ilovemj", password_confirmation: 'ilovemj')
+      @user.first_name = nil
 
       expect(@user).to_not be_valid
       expect(@user.errors.full_messages).to include("First name can't be blank")
     end
 
     it "last name must be present" do
-      @user = User.create(first_name: "Peter", last_name: nil, email: "spider@man.com", password: "ilovemj", password_confirmation: 'ilovemj')
-
+      @user.last_name = nil
+      
       expect(@user).to_not be_valid
       expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
 
     it "password must be at least 6 characters" do
-      @user = User.create(first_name: "Peter", last_name: "Parker", email: "spider@man.com", password: "123", password_confirmation: '123')
+      @user.password = 'short'
+      @user.password_confirmation = 'short'
 
       expect(@user).to_not be_valid
       expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
